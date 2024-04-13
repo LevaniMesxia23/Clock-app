c2e8be1519cc97b09171fc6f2c6a18de1d919256
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2024-04-13 12:24:11 +0400

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
-
+import styled from "styled-components"
+import Header from "./components/Header"
 function App() {
 
   return (
     <>
-
+    <Container>
+      <Header />
+    </Container>
+    
     </>
   )
 }
 
 export default App
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
